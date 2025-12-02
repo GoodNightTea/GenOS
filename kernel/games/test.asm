298f7a73db82642f7033c09d7a739c4320504bc6
GoodNightTea <82637249+GoodNightTea@users.noreply.github.com>
2025-12-02 14:55:17 +0100

    Update test.asm

--- kernel/games/test.asm
+++ kernel/games/test.asm
@@ -19,7 +19,7 @@ test:
 	inc dword [colord]
 	add dword [x], 15
 	jmp .display
-.next_row:
+.next_row:	
 	
 	mov dword [x], 0
 	add dword [y], 15
@@ -39,21 +39,141 @@ test:
 	mov al, 1
 	call mode13_clear_screen
 .loop:
-	
-	; wait for input
-
+	mov eax, dword [freq]
+	hlt
 	call play_tone
 	mov eax, 1
 	call wait_frames
 	call stop_tone
+	hlt
 	mov eax, 1
 	call wait_frames
 	hlt
-	hlt
-	mov eax, dword [freq]
+	
+	
 	jmp .loop
 	
-	
+NOTE_E5  equ 659
+NOTE_B4  equ 494
+NOTE_C5  equ 523
+NOTE_D5  equ 587
+NOTE_A4  equ 440
+NOTE_G4  equ 392
+
+QUARTER  equ 30    
+EIGHTH   equ 15    
+DOTTED_Q equ 45    
+SIXTEENTH equ 8    
+
+play_tetris:
+
+    mov eax, NOTE_E5
+    call play_tone
+    mov eax, QUARTER
+    call wait_frames
+    
+    mov eax, NOTE_B4
+    call play_tone
+    mov eax, EIGHTH
+    call wait_frames
+    
+    mov eax, NOTE_C5
+    call play_tone
+    mov eax, EIGHTH
+    call wait_frames
+    
+    mov eax, NOTE_D5
+    call play_tone
+    mov eax, QUARTER
+    call wait_frames
+    
+    mov eax, NOTE_C5
+    call play_tone
+    mov eax, EIGHTH
+    call wait_frames
+    
+    mov eax, NOTE_B4
+    call play_tone
+    mov eax, EIGHTH
+    call wait_frames
+    
+
+    mov eax, NOTE_A4
+    call play_tone
+    mov eax, QUARTER
+    call wait_frames
+    
+    mov eax, NOTE_A4
+    call play_tone
+    mov eax, EIGHTH
+    call wait_frames
+    
+    mov eax, NOTE_C5
+    call play_tone
+    mov eax, EIGHTH
+    call wait_frames
+    
+    mov eax, NOTE_E5
+    call play_tone
+    mov eax, QUARTER
+    call wait_frames
+    
+    mov eax, NOTE_D5
+    call play_tone
+    mov eax, EIGHTH
+    call wait_frames
+    
+    mov eax, NOTE_C5
+    call play_tone
+    mov eax, EIGHTH
+    call wait_frames
+    
+
+    mov eax, NOTE_B4
+    call play_tone
+    mov eax, QUARTER
+    call wait_frames
+    
+    call stop_tone
+    mov eax, EIGHTH
+    call wait_frames
+    
+    mov eax, NOTE_C5
+    call play_tone
+    mov eax, EIGHTH
+    call wait_frames
+    
+    mov eax, NOTE_D5
+    call play_tone
+    mov eax, QUARTER
+    call wait_frames
+    
+    mov eax, NOTE_E5
+    call play_tone
+    mov eax, QUARTER
+    call wait_frames
+    
+
+    mov eax, NOTE_C5
+    call play_tone
+    mov eax, QUARTER
+    call wait_frames
+    
+    mov eax, NOTE_A4
+    call play_tone
+    mov eax, QUARTER
+    call wait_frames
+    
+    mov eax, NOTE_A4
+    call play_tone
+    mov eax, QUARTER
+    call wait_frames
+    
+    call stop_tone
+    mov eax, QUARTER
+    call wait_frames
+    
+    ret
 
 x 		dd 0
 y 		dd 0
