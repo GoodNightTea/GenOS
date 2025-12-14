b4a14a35eec46b803a10916e90fe46dbc50806c1
GoodNightTea <82637249+GoodNightTea@users.noreply.github.com>
2025-12-14 16:32:43 +0100

    Update global_functions.asm
    tomfoolery

--- kernel/games/global_functions.asm
+++ kernel/games/global_functions.asm
@@ -109,8 +109,96 @@ xorshift32:
     pop ebx
     ret
 
+byte_to_hex:
+    push eax
+    push ebx
+    
+    ; High nibble
+    mov bl, al
+    shr bl, 4
+    and bl, 0x0F
+    cmp bl, 9
+    jle .high_digit
+    add bl, 'A' - 10
+    jmp .high_done
+.high_digit:
+    add bl, '0'
+.high_done:
+    mov [hex_buf], bl
+    
+    ; Low nibble
+    mov bl, al
+    and bl, 0x0F
+    cmp bl, 9
+    jle .low_digit
+    add bl, 'A' - 10
+    jmp .low_done
+.low_digit:
+    add bl, '0'
+.low_done:
+    mov [hex_buf + 1], bl
+    mov byte [hex_buf + 2], 0
+    
+    mov esi, hex_buf
+    pop ebx
+    pop eax
+    ret
 
-
+init_pics:
+    ; ICW1: Initialize both PICs
+    mov al, 0x11                ; ICW1: Init + ICW4 needed
+    out 0x20, al                ; Send to PIC1 command port
+    out 0xA0, al                ; Send to PIC2 command port
+    
+    ; ICW2: Set interrupt vector offsets
+    mov al, 0x20                ; PIC1 starts at INT 32 (0x20)
+    out 0x21, al                ; PIC1 data port
+    mov al, 0x28                ; PIC2 starts at INT 40 (0x28)
+    out 0xA1, al                ; PIC2 data port
+    
+    ; ICW3: Setup cascade
+    mov al, 0x04                ; PIC1: slave on IRQ2
+    out 0x21, al
+    mov al, 0x02                ; PIC2: cascade identity
+    out 0xA1, al
+    
+    ; ICW4: Set mode
+    mov al, 0x01                ; 8086 mode
+    out 0x21, al
+    out 0xA1, al
+    
+	; OCW1 interrupt masks
+	mov al, 0xFC                ; 11111100b - enable ONLY IRQ0, IRQ1
+	out 0x21, al
+	mov al, 0xFF                ; Mask all on PIC2
+	out 0xA1, al
+    
+    ret
+; ============================================================================
+; setup_fdc_idt: Set up IDT entry for FDC interrupt (IRQ6 = INT 38)
+; ============================================================================
+setup_fdc_idt:
+    pushad
+    
+    ; Calculate absolute address of handler
+    mov eax, 0x100000
+    mov ebx, fdc_irq_handler
+    sub ebx, kernel_entry
+    add eax, ebx
+    
+    ; Point to IDT entry 38 (IRQ6)
+    mov edi, 0x110000
+    add edi, (38 * 8)
+    
+    mov word [edi], ax
+    mov word [edi + 2], 0x08
+    mov byte [edi + 4], 0
+    mov byte [edi + 5], 0x8E
+    shr eax, 16
+    mov word [edi + 6], ax
+    
+    popad
+    ret
 ;==========GLOBAL STRINGS/VARIABLES======
 paused 	      db 'PAUSED', 0
 	
