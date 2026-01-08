885ad666ed56d9eda89c970c19906c26b9e28f85
GoodNightTea <82637249+GoodNightTea@users.noreply.github.com>
2026-01-08 11:36:25 +0100

    Update main_kernel.asm
    wrong file bru

--- kernel/main_kernel.asm
+++ kernel/main_kernel.asm
@@ -1,20 +1,31 @@
-
 [BITS 32]
 [ORG 0x100000]
 kernel_entry:
     mov esp, 0x7C00
-    cli
 
+    cli
     call setup_idt
     call setup_timer_idt
     call init_pics
-    call init_timer
-	
-    sti
-	;call fdc_init
 
-	;call fat12_init
-    
+    call init_timer
+	sti
+	cmp byte [menu_choice], -1
+	je .entry
+	
+    call setup_fdc_idt      
+	call init_fdc
+	call show_disk_stats
+	call show_disk_layout
+	mov eax, 1
+	call wait_frames
+.entry:
+	cli
+    ; god thats a sloppy fix, can just add an in_game variable but idk im lazy
+    mov byte [is_tetris], 0
+    mov byte [is_pong], 0  
+    mov byte [is_debug], 0
+    mov byte [is_cli], 0
     mov al, 0
     call mode13_clear_screen
 
@@ -48,14 +59,19 @@ kernel_entry:
 	add ebx, 10
     mov dl, 5
     call mode13_print_string
+    mov esi, TERMINAL
+	add ebx, 10
+    mov dl, 5
+    call mode13_print_string
     
     mov esi, DEBUG
     add ebx, 10
     mov dl, 6
     call mode13_print_string
-    mov byte [menu_choice], 0xff
+    mov byte [menu_choice], -1
 
 .wait_for_choice:
+	sti
     hlt                              ; Wait for keyboard interrupt
     mov al, [menu_choice]            ; Check what user pressed
     cmp al, 1
@@ -101,6 +117,20 @@ kernel_entry:
 
 
 
+; ============================================================================
+; Disk Layout Constants (injected by build script)
+; ============================================================================
+%ifndef DATA_START_SECTOR
+    %define DATA_START_SECTOR 34   
+%endif
+
+%ifndef DATA_SECTORS
+    %define DATA_SECTORS 2846      
+%endif
+
+
+KERNEL_DATA_START:  dd DATA_START_SECTOR
+KERNEL_DATA_SIZE:   dd DATA_SECTORS
 
 ; ============================================================================
 ; Include Kernels
@@ -126,20 +156,34 @@ kernel_entry:
 %include "drivers/audio/audio_driver.asm"
 %include "drivers/fs/fat12.asm"
 %include "drivers/fdc/floppydisk_controller.asm"
+%include "drivers/dma/direct_mem_access.asm"
 ; ============================================================================
 ; Data Section
 ; ============================================================================
-	 
+ sector0_label: db 'UNO:', 0
+sector1_label: db 'DOS:', 0
+sector2_label: db 'TRES:', 0
+read_fail:     db 'FUCK', 0
 
-menu		  db 'CHOOSE A GAME', 0
+menu		      db 'CHOOSE A GAME', 0
 SNAKE		  db '1 SNAKE', 0
 TETRIS		  db '2 TETRIS', 0
 GOF       	  db '3 GAMEOFLIFE', 0
-PONG		  db '4 PONG', 0
+PONG		      db '4 PONG', 0
 PACMAN		  db '5 PACMAN', 0
+TERMINAL		  db '6 TERMINAL', 0
 DEBUG		  db '0 DEBUG', 0
+
+
+TOTAL_DISK_SECTORS  equ 2880
+KERNEL_START_SECTOR equ 2
+
+
 
 ; IDT structures
 idt_desc:
     dw 2047                         ; Limit (256 entries * 8 bytes - 1)
     dd 0                            ; Base address (filled in by setup_idt)
+
+WRITE_BUFFER    equ 0x200000
+READ_BUFFER     equ 0x200200   ; 512 bytes after write buffer
