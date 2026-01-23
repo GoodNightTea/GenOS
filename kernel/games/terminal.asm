787ecda544fd2023373b5976b0df85a634498d99
GoodNightTea <82637249+GoodNightTea@users.noreply.github.com>
2026-01-23 10:39:26 +0100

    Refactor terminal input handling and buffer logic
    fixed the differentiation between delete and space, i set space as colour 0 which I used to differentiate between the two which caused the delete to function as a secondary space bar

--- kernel/games/terminal.asm
+++ kernel/games/terminal.asm
@@ -3,24 +3,26 @@ terminal:
     mov al, 0
     call mode13_clear_screen
     ; todo: curser? cursor? idk just to see where you are typing
-    mov dword [column], 0
+    mov dword [column], 2
     mov dword [row], 1
     mov dword [colour], 15
     mov dword [buffer_offset], 0
     mov dword [current_sector], 0
+    mov dword [delete_request], 0
     mov byte [flush_request], 0
     ; Clear the write buffer
     mov edi, WRITE_BUFFER
-    mov ecx, 512
+    mov ecx, 32
     xor al, al
     rep stosb
     	
 .main:
     sti
     hlt                             
-    
+ 
 	cmp byte [flush_request], 1
 	je .flush
+	
 .handle_input:
 
     cmp byte [input_buffer], 0xff
@@ -48,28 +50,25 @@ terminal:
     mov esi, input_buffer        
     call mode13_print_string
     pop eax
+
+    cmp byte [colour], 0
+    je .delete
     
-    ; ===== FIXED: Only buffer in RAM, don't write to disk yet =====
     mov al, [input_buffer]
     mov edi, WRITE_BUFFER
     add edi, [buffer_offset]
-    stosb                           ; Store byte
-    
+    stosb
     inc dword [buffer_offset]
     
-    ; Auto-flush if buffer full
-    cmp dword [buffer_offset], 32  ; Full sector
+    cmp dword [buffer_offset], 32
     jge .flush
-    jmp .continue
     
-.flush:
-    call flush_buffer
-    mov byte [flush_request], 0 ; flush request handled
-    jmp .skip_advance
+    inc dword [column]
+    jmp .reset   
     
 .continue:
-    cmp byte [colour], 0
-    je .skip_advance
+    cmp byte [colour], 1
+    je .delete
     inc dword [column]
     
 .skip_advance:
@@ -77,16 +76,37 @@ terminal:
     mov byte [colour], 15
     
     ; Check for line wrap
-    cmp dword [column], 38
+    cmp dword [column], 32
     jge .next_row
     jmp .main
  
+.delete:
+	cmp dword [buffer_offset], 0
+	je .reset
+
+	dec dword [buffer_offset]
+	jmp .reset
+    
+.flush:
+    call flush_buffer
+    mov byte [flush_request], 0 ; flush request handled
+    inc dword [column]
+    jmp .continue
    
 .next_row:
     mov dword [column], 1
     inc dword [row]
     jmp .main
 
+.reset:
+    mov byte [input_buffer], 0xff
+    mov byte [colour], 15
+    
+    cmp dword [column], 32
+    je .next_row   
+    jmp .main
+    
+    
 flush_buffer:
 	pushad ; turned to true global 
     ; Write full buffer to disk
@@ -117,3 +137,4 @@ column      dd 0
 input_buffer: db 0, 0
 current_sector dd 0
 flush_request:	db 0
+delete_request	dd 0	
