350370ce44d42ef02b11a04e6c647bc12938d132
GoodNightTea <82637249+GoodNightTea@users.noreply.github.com>
2026-01-23 15:24:50 +0100

    Create font_tester.asm

---
+++ kernel/misc/font_tester.asm
@@ -0,0 +1,24 @@
+font_tester:
+	mov al, 1
+	call mode13_clear_screen
+	mov esp, 0x7C00
+
+    mov esi, alphabet
+    mov eax, 20
+    mov ebx, 10
+    mov dl, 100
+    call mode13_print_string
+    
+    mov esi, alphabet
+    mov eax, 20
+    mov ebx, 30
+    mov dl, 100
+    call font2_string
+    
+.loop:
+	mov eax, 2000
+	call wait_frames
+	jmp .loop
+	
+	
+alphabet		      db '0123456789 ABCDEFGHIJKLMNOPQRSTUVW', 0
