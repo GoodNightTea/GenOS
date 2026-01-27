8f23471867032357a92eaac29db58dfc27afc47d
GoodNightTea <82637249+GoodNightTea@users.noreply.github.com>
2026-01-27 11:20:04 +0100

    Implement gradient rectangle drawing function
    Added a function to draw a gradient rectangle with a specified color range.

--- kernel/drivers/vga/13h_vga.asm
+++ kernel/drivers/vga/13h_vga.asm
@@ -521,13 +521,79 @@ mode13_pong:
 	pop eax
 	pop ebx
     ret
+   
+
+; ----------------------------------------------------------------------------
+; draw_gradient_rect: draws a gradient rectangle with with a ranging gradient scale
+; Input: EAX = x, EBX = y, ECX = width, EDX = height
+;   [grad_start_idx] = first palette index to use
+;   [grad_end_idx] = last palette index to use
+; ----------------------------------------------------------------------------
+
+
+draw_gradient_rect:
+    pushad
+    
+    mov [grad_x], eax
+    mov [grad_y], ebx
+    mov [grad_width], ecx
+    mov [grad_height], edx
+    
+    ; Calculate palette range
+    mov eax, [grad_end_idx]
+    sub eax, [grad_start_idx]
+    mov [grad_range], eax           ; Number of colors available
+    
+    xor ebp, ebp                    ; Row counter
+    
+.row_loop:
+    cmp ebp, [grad_height]
+    jge .done
+    
+    xor esi, esi                    ; Column counter
+    
+.col_loop:
+    cmp esi, [grad_width]
+    jge .next_row
+    
+
+    ; index = start + (column * range) / width
+    mov eax, esi
+    mul dword [grad_range]          ; EDX:EAX = column * range
+    div dword [grad_width]          ; EAX = (column * range) / width
+    add eax, [grad_start_idx]       ; Add start offset
+    
+    ; Clamp to end index (safety)
+    cmp eax, [grad_end_idx]
+    jle .in_range
+    mov eax, [grad_end_idx]
+.in_range:
+    
+    ; Draw pixel
+    push eax                        ; Save color
+    mov eax, [grad_x]
+    add eax, esi
+    mov ebx, [grad_y]
+    add ebx, ebp
+    pop ecx                         ; Color index
+    call mode13_set_pixel
+    
+    inc esi
+    jmp .col_loop
+    
+.next_row:
+    inc ebp
+    jmp .row_loop
+    
+.done:
+    popad
+    ret
 
 
 ; ----------------------------------------------------------------------------
 ; mode13_rgb_rect: Draw filled rectangle with changing color per pixel
 ; Input: EAX = x, EBX = y, ECX = width, EDX = height, ESI = starting color
 ; ----------------------------------------------------------------------------
-
 mode13_rgb_rect:
     pushad
     
@@ -565,6 +631,7 @@ mode13_rgb_rect:
 .done:
     popad
     ret
+    
 ; ----------------------------------------------------------------------------
 ; draw_border: Draws snake border
 ; ----------------------------------------------------------------------------
@@ -606,9 +673,190 @@ draw_border:
     popad
     ret
 
+
+
+; ============================================================================
+; Gradient setups - created with the help of generative AI
+; ============================================================================
+
+; Grayscale in 16-47
+setup_grayscale_palette:
+    pushad
+    xor edi, edi
+.loop:
+    cmp edi, 32
+    jge .done
+    
+    mov eax, edi
+    mov edx, 63
+    mul edx
+    mov ebx, 31
+    xor edx, edx
+    div ebx                         ; Intensity
+    
+    mov ebx, eax                    ; R=G=B
+    mov ecx, eax
+    mov eax, edi
+    add eax, 16                     ; Index 16-47
+    mov bh, bl
+    call setup_grayscale_palette
+    
+    inc edi
+    jmp .loop
+.done:
+    popad
+    ret
+
+; Rainbow in 48-79
+setup_rainbow_palette:
+    pushad
+    xor edi, edi
+.loop:
+    cmp edi, 32
+    jge .done
+    
+
+    ; Hue varies from 0-360 degrees
+    mov eax, edi
+    mov edx, 360
+    mul edx
+    mov ebx, 32
+    div ebx                         ; EAX = hue (0-360)
+    
+
+    cmp eax, 120
+    jl .red_to_green
+    cmp eax, 240
+    jl .green_to_blue
+    ; blue_to_red
+    sub eax, 240
+    mov ebx, 63                     ; Red increases
+    mul ebx
+    mov ecx, 120
+    div ecx
+    mov ebx, eax
+    mov ecx, 63
+    sub ecx, eax                    ; Blue decreases
+    xor eax, eax                    ; Green = 0
+    jmp .set
+    
+.red_to_green:
+    mov ebx, 63                     ; Red = max
+    mov edx, eax
+    mov eax, 63
+    mul edx
+    mov ecx, 120
+    div ecx                         ; Green increases
+    mov [temp_g], al
+    mov ecx, 0                      ; Blue = 0
+    mov al, [temp_g]
+    mov bh, al
+    jmp .set
+    
+.green_to_blue:
+    sub eax, 120
+    xor ebx, ebx                    ; Red = 0
+    mov edx, 63
+    sub edx, eax
+    mov bh, dl                      ; Green decreases
+    mov ecx, eax                    ; Blue increases
+    
+.set:
+    push ebx
+    push ecx
+    mov eax, edi
+    add eax, 48                     ; Index 48-79
+    pop ecx
+    pop ebx
+    call set_palette_color
+    
+    inc edi
+    jmp .loop
+.done:
+    popad
+    ret
+
+
+setup_gradient_palette:
+    pushad
+    
+    xor edi, edi                    ; Counter: 0-31
+    
+.loop:
+    cmp edi, 32
+    jge .done
+    
+    ; Calculate intensity: (counter * 63) / 31
+    mov eax, edi
+    mov edx, 63
+    mul edx                         ; EDX:EAX = counter * 63
+    mov ebx, 31
+    div ebx                         ; EAX = intensity (0-63)
+    
+    ; Palette index = 16 + counter
+    mov ebx, edi
+    add ebx, 16
+    
+    ; Set palette entry
+    push eax                        ; Save intensity
+    push ebx                        ; Save index
+    
+    mov al, bl                      ; AL = palette index
+    pop ebx                         ; Get index back
+    pop ebx                         ; Get intensity
+    
+    ; Set R=G=B=intensity for grayscale
+    mov bl, bl                      ; Red = intensity (already in BL)
+    mov bh, bl                      ; Green = intensity
+    mov cl, bl                      ; Blue = intensity
+    
+    push edi
+    call set_palette_color
+    pop edi
+    
+    inc edi
+    jmp .loop
+    
+.done:
+    popad
+    ret
+ 
+set_palette_color:
+    ; Input: AL = index, BL = red (0-63), BH = green (0-63), CL = blue (0-63)
+    push eax
+    push ebx
+    push ecx
+    push edx
+    
+    ; Select palette index
+    mov dx, 0x03C8
+    out dx, al
+    
+    ; Write RGB values
+    mov dx, 0x03C9
+    mov al, bl              ; Red
+    out dx, al
+    mov al, bh              ; Green
+    out dx, al
+    mov al, cl              ; Blue
+    out dx, al
+    
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
+    ret
 ; ============================================================================
 ; DATA SECTION FOR 13h VGA DRIVER
 ; ============================================================================
+temp_g: db 0
+grad_x:         dd 0
+grad_y:         dd 0
+grad_width:     dd 0
+grad_height:    dd 0
+grad_start_idx: dd 16               
+grad_end_idx:   dd 47
+grad_range:     dd 0
 
 m13_rect_x:      dd 0
 m13_rect_y:      dd 0
