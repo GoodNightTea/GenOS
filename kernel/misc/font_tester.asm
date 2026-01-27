7bcd9e18c49522311cc6ab7309168e5721438059
GoodNightTea <82637249+GoodNightTea@users.noreply.github.com>
2026-01-27 11:20:25 +0100

    Update font_tester.asm

--- kernel/misc/font_tester.asm
+++ kernel/misc/font_tester.asm
@@ -1,24 +1,45 @@
 font_tester:
-	mov al, 1
-	call mode13_clear_screen
-	mov esp, 0x7C00
+	mov esp, 0x7C00	
 
-    mov esi, alphabet
-    mov eax, 20
-    mov ebx, 10
-    mov dl, 100
+	mov dword [grad_start_idx], 16
+	mov dword [grad_end_idx], 32
+	mov eax, 0
+	mov ebx, 0
+	mov ecx, 320
+	mov edx, 200
+	call draw_gradient_rect
+	
+	mov esi, alphabet
+    mov eax, 10
+    mov ebx, 15
+    mov dl, 15
     call mode13_print_string
     
-    mov esi, alphabet
-    mov eax, 20
-    mov ebx, 30
-    mov dl, 100
+    mov esi, first
+    mov eax, 10
+    mov ebx, 5
+    mov dl, 15
+    call mode13_print_string
+   
+    mov esi, second
+    mov eax, 10
+    mov ebx, 35
+    mov dl, 15
     call font2_string
-    
+
+
+    mov esi, alphabet
+    mov eax, 10
+    mov ebx, 45
+    mov dl, 15
+    call font2_string
+
 .loop:
 	mov eax, 2000
 	call wait_frames
 	jmp .loop
 	
-	
-alphabet		      db '0123456789 ABCDEFGHIJKLMNOPQRSTUVWXYZ', 0
+numbers			  db '0123456789'
+alphabet		  db 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 0
+first		      db 'first font', 0
+second		      db 'second font', 0
