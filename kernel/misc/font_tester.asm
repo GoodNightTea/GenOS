95405639b31523f044f67a2b9ff39d0175b7f9e5
GoodNightTea <82637249+GoodNightTea@users.noreply.github.com>
2026-01-29 13:28:10 +0100

    Update font_tester.asm

--- kernel/misc/font_tester.asm
+++ kernel/misc/font_tester.asm
@@ -1,14 +1,27 @@
 font_tester:
 	mov esp, 0x7C00	
-
-	mov dword [grad_start_idx], 16
-	mov dword [grad_end_idx], 32
+  	pushad
+  	
+	mov byte [grad_start_r], 63    ; Orange
+	mov byte [grad_start_g], 32
+	mov byte [grad_start_b], 0
+	mov byte [grad_end_r], 40      ; Purple
+	mov byte [grad_end_g], 0
+	mov byte [grad_end_b], 50
+	mov dword [grad_palette_start], 16
+	mov dword [grad_num_colors], 240
+	call setup_custom_gradient
 	mov eax, 0
-	mov ebx, 0
-	mov ecx, 320
-	mov edx, 200
+   	mov ebx, 0
+   	mov ecx, 320
+   	mov edx, 200
+	mov dword [grad_start_idx], 16
+	mov dword [grad_end_idx], 240
 	call draw_gradient_rect
-	
+	mov eax, 10
+    call wait_frames
+    
+	popad
 	mov esi, alphabet
     mov eax, 10
     mov ebx, 15
